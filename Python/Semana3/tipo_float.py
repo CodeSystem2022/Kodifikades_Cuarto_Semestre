57d8b41a84d094e88f91d6fbb754e567ee3ef1b1
Lucas <lucasmruiz05@gmail.com>
2023-09-14 19:45:37 -0300

    tipos float

---
+++ Python/Semana3/tipo_float.py
@@ -0,0 +1,12 @@
+# Profundizando tipo float
+a = 3.0
+print(f'a: {a:.2f}')
+
+# Constructor de tipo float -> puede recibir enteros y de tipo string
+a = float(10)  # Le pasamos un tipo entero (int)
+b = float('10')  # Le pasamos un tipo string
+print(f'a: {a:.2f}')
+
+# Notacion exponencial (valores positivos o negativos)
+a = 3e5
+print(f'a: {a}')
