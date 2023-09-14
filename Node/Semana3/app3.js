ffe3897465591d955bcd7c7492792e72907e1c23
Lucas <lucasmruiz05@gmail.com>
2023-09-14 19:58:40 -0300

    settimeout

---
+++ Node/Semana3/app3.js
@@ -0,0 +1,13 @@
+console.log('Init app!');
+
+setTimeout(() => {
+	console.log('First Timeout');
+}, 1000);
+setTimeout(() => {
+	console.log('Second Timeout');
+}, 5000);
+setTimeout(() => {
+	console.log('Thirds Timeout');
+}, 10000);
+
+console.log('App finished!');
