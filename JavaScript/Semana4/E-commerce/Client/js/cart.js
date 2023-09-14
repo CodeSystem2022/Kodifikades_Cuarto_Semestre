8c078e2e3e436608c595fba8913a45c4d80d0aff
Lucas <lucasmruiz05@gmail.com>
2023-09-14 20:12:08 -0300

    footer

--- JavaScript/Semana4/E-commerce/Client/js/cart.js
+++ JavaScript/Semana4/E-commerce/Client/js/cart.js
@@ -1,40 +1,38 @@
-const modalContainer=document.getElementById("modal-container");
-const modalOverlay= document.getElementById("modal-overlay");
+const modalContainer = document.getElementById('modal-container');
+const modalOverlay = document.getElementById('modal-overlay');
 
-const cartBtn= document.getElementById("cart-btn");
+const cartBtn = document.getElementById('cart-btn');
 
+const displayCart = () => {
+	modalContainer.innerHTML = '';
+	modalContainer.style.display = 'block';
+	modalOverlay.style.display = 'block';
+	//modal header
+	const modalHeader = document.createElement('div');
 
+	const modalClose = document.createElement('div');
 
-const displayCart =() =>{
-    modalContainer.innerHTML="";
-    modalContainer.style.display="block";
-    modalOverlay.style.display="block";
-    //modal header
-    const modalHeader=document.createElement("div");
+	modalClose.innerText = '❌';
+	modalClose.className = 'modal-close';
+	modalHeader.append(modalClose);
 
-    const modalClose = document.createElement("div");
+	modalClose.addEventListener('click', () => {
+		modalContainer.style.display = 'none';
+		modalOverlay.style.display = 'none';
+	});
 
-    modalClose.innerText="❌";
-    modalClose.className= "modal-close";
-    modalHeader.append(modalClose)
+	const modalTitle = document.createElement('div');
+	modalTitle.innerText = 'Cart';
+	modalTitle.className = 'modal-title';
+	modalHeader.append(modalTitle);
 
-    modalClose.addEventListener("click",()=>{
-        modalContainer.style.display="none";
-        modalOverlay.style.display="none";
-    });
+	modalContainer.append(modalHeader);
 
-    const modalTitle= document.createElement("div");
-    modalTitle.innerText="Cart";
-    modalTitle.className="modal-title";
-    modalHeader.append(modalTitle);
-
-    modalContainer.append(modalHeader);
-
-    // modal body
-    cart.forEach((product) =>{
-    const modalBody= document.createElement("div");
-    modalBody.className="modal-body";
-    modalBody.innerHTML= `
+	// modal body
+	cart.forEach((product) => {
+		const modalBody = document.createElement('div');
+		modalBody.className = 'modal-body';
+		modalBody.innerHTML = `
     <div class="product">
         <img class="product-img" src="${product.img}" />
         <div class="product-info">
@@ -49,45 +47,45 @@ const displayCart =() =>{
             <div class="delete-product">❌</div>
     </div>
     `;
-    modalContainer.append(modalBody);
-    
-    const decrese = modalBody.querySelector(".quantity-btn-decrese");
-    decrese.addEventListener("click", () => {
-        if(product.quanty !==1){
-        product.quanty--;
-        displayCart();
-    }
-    });
+		modalContainer.append(modalBody);
 
-    const increse = modalBody.querySelector(".quantity-btn-increse");
-    increse.addEventListener("click", () => {
-        product.quanty++;
-        displayCart();
-    });
+		const decrese = modalBody.querySelector('.quantity-btn-decrese');
+		decrese.addEventListener('click', () => {
+			if (product.quanty !== 1) {
+				product.quanty--;
+				displayCart();
+			}
+		});
 
-    // delete
-    const deleteProduct = modalBody.querySelector(".delete-product");
+		const increse = modalBody.querySelector('.quantity-btn-increse');
+		increse.addEventListener('click', () => {
+			product.quanty++;
+			displayCart();
+		});
 
-    deleteProduct.addEventListener("click", () => {
-        deleteCartProduct(product.id);
-    });
- });
-    
-    // modal fotter
-    const total = cart.reduce((acc, el) => acc + el.price * el.quanty, 0);
+		// delete
+		const deleteProduct = modalBody.querySelector('.delete-product');
 
-    const modalFooter= document.createElement("div");
-    modalFooter.className="modal-footer"
-    modalFooter.innerHTML=`
+		deleteProduct.addEventListener('click', () => {
+			deleteCartProduct(product.id);
+		});
+	});
+
+	// modal footer
+	const total = cart.reduce((acc, el) => acc + el.price * el.quanty, 0);
+
+	const modalFooter = document.createElement('div');
+	modalFooter.className = 'modal-footer';
+	modalFooter.innerHTML = `
     <div class="total-price">Total: ${total}</div>
     `;
-    modalContainer.append(modalFooter);
-}; 
+	modalContainer.append(modalFooter);
+};
 
-cartBtn.addEventListener("click", displayCart);
+cartBtn.addEventListener('click', displayCart);
 
-const deleteCartProduct =(id)=> {
-    const foundId = cart.findIndex((element)=> element.id === id);
-    cart.splice(foundId, 1);
-    displayCart();
-}
+const deleteCartProduct = (id) => {
+	const foundId = cart.findIndex((element) => element.id === id);
+	cart.splice(foundId, 1);
+	displayCart();
+};
