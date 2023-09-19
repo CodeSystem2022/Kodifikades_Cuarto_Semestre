75dfa745bbd55147db2eb1732f0963cdef205df4
Eduardo Bru <113075379+EduardoBru@users.noreply.github.com>
2023-09-19 18:04:52 -0300

    2.2 Código de carrito

---
+++ JavaScript/Semana3/E-commerce/Client/js/products.js
@@ -0,0 +1,45 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 1000,
+        quanty: 1,
+        img: "../client/media/banana.png",
+    },
+    {
+        id: 2,
+        productName: "Enlatados",
+        price: 600,
+        quanty: 1,
+        img: "../client/media/enlatados.png",
+    },
+    {
+        id: 3,
+        productName: "Leche",
+        price: 1500,
+        quanty: 3,
+        img: "../client/media/leche.png",
+    },
+    {
+        id: 5,
+        productName: "Mayonesa",
+        price: 2000,
+        quanty: 5,
+        img: "../client/media/mayonesa.png",
+    },
+
+    {
+        id: 7,
+        productName: "Pollo",
+        price: 4000,
+        quanty: 5,
+        img: "../client/media/pollo.png",
+    },
+    {
+        id: 8,
+        productName: "Varios",
+        price: 10000,
+        quanty: 1,
+        img: "../client/media/varios.png",
+    },
+];
