012ca485da9ca8d583db86c102a2025fd420c074
Eduardo Bru <113075379+EduardoBru@users.noreply.github.com>
2023-09-19 17:25:43 -0300

    2.2 Seguimos con -> Decimal, Binario, Octal y Hexadecimal Ahora con int('valor', base)

--- Python/Semana3/Sistemas_Numeracion.py
+++ Python/Semana3/Sistemas_Numeracion.py
@@ -1,39 +1,39 @@
-# Profundizando en los sistemas de numeración
+# Profundizando en sistemas de numeración
 
 # Sistema decimal
 a = 10
-print(f'a decimal: {a}')
+print(f"a Decimal: {a}")
 
-# Siatema Binario
+# Sistema Binario
 a = 0b1010
-print(f'a binario: {a}')
+print(f"a Binario: {a}")
 
 # Sistema Octal
 a = 0o12
-print(f'a octal: {a}')
+print(f"a Octal: {a}")
 
-# Sistema Exadecimal
+# Sistema Hexadecimal
 a = 0xA
-print(f'a exadecinal: {a}')
+print(f"a Hexadecimal: {a}")
 
 # Base Decimal
-a = int('23', 10)
-print(f'a = base decimal: {a}')
+a = int("23", 10)
+print(f"a base decimal: {a}")
 
 # Base Binario
-a = int('1010', 2)
-print(f'a = base binario: {a}')
+a = int("10111", 2)
+print(f"a base binario: {a}")
 
-# base Octal
-a = int('12', 8)
-print(f'a base octal: {a}')
+# Base Octal
+a = int("27", 8)
+print(f"a = base octal: {a}")
 
-# Base Exadecimal
-a = int('A', 16)
-print(f'a base exadecinal: {a}')
+# Base Hexadecimal
+a = int("17", 16)
+print(f"a = base hexadecimal: {a}")
 
 # Base 5 sus valores son de 0 a 4
-a = int('34', 5)
-print(f'a base 5: {a}')
+a = int("34", 5)
+print(f"a Base 5: {a}")
 
 
