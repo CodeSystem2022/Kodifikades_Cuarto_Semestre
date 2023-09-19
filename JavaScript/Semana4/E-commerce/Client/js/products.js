8e8bff1c8bee4ae7a9290578abeef9e7e87a9962
Eduardo Bru <113075379+EduardoBru@users.noreply.github.com>
2023-09-19 18:19:44 -0300

    3.6 Eliminar productos del modal -> Parte 1 y 2

--- JavaScript/Semana4/E-commerce/Client/js/products.js
+++ JavaScript/Semana4/E-commerce/Client/js/products.js
@@ -1,32 +1,45 @@
-const productos=[
-{
-    id: 1,
-    productName: "Banana",
-    price: 480,
-    quanty: 1,
-    img: "../media/banana.png"
-},
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 1000,
+        quanty: 1,
+        img: "../client/media/banana.png",
+    },
+    {
+        id: 2,
+        productName: "Enlatados",
+        price: 600,
+        quanty: 1,
+        img: "../client/media/enlatados.png",
+    },
+    {
+        id: 3,
+        productName: "Leche",
+        price: 1500,
+        quanty: 3,
+        img: "../client/media/leche.png",
+    },
+    {
+        id: 5,
+        productName: "Mayonesa",
+        price: 2000,
+        quanty: 5,
+        img: "../client/media/mayonesa.png",
+    },
 
-{
-    id:2,
-    productName: "Leche",
-    price: 950,
-    quanty: 1,
-    img: "../media/leche.png"
-
-},
-{
-    id:3,
-    productName:"Pollo",
-    price: 750,
-    quanty: 1,
-    img:"../media/pollo.png"
-
-},
-{
-    id:4,
-    productName:"Mayonesa",
-    price: 510,
-    quanty: 1,
-    img:"../media/mayonesa.png"
-}]
+    {
+        id: 7,
+        productName: "Pollo",
+        price: 4000,
+        quanty: 5,
+        img: "../client/media/pollo.png",
+    },
+    {
+        id: 8,
+        productName: "Varios",
+        price: 10000,
+        quanty: 1,
+        img: "../client/media/varios.png",
+    },
+];
