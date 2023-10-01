6aae8f7eb7abf1cb02549813ebe3f5edf141a4e7
Lucas <lucasmruiz05@gmail.com>
2023-10-01 17:26:04 -0300

    fix problems

--- JavaScript/Semana4/E-commerce/Client/js/cart.js
+++ JavaScript/Semana4/E-commerce/Client/js/cart.js
@@ -7,18 +7,11 @@ const displayCart = () => {
 	modalContainer.innerHTML = '';
 	modalContainer.style.display = 'block';
 	modalOverlay.style.display = 'block';
-<<<<<<< Updated upstream
 	//modal header
 	const modalHeader = document.createElement('div');
 
 	const modalClose = document.createElement('div');
 
-=======
-	//modal Header
-	const modalHeader = document.createElement('div');
-
-	const modalClose = document.createElement('div');
->>>>>>> Stashed changes
 	modalClose.innerText = '❌';
 	modalClose.className = 'modal-close';
 	modalHeader.append(modalClose);
@@ -35,60 +28,6 @@ const displayCart = () => {
 
 	modalContainer.append(modalHeader);
 
-<<<<<<< Updated upstream
-	// modal body
-	cart.forEach((product) => {
-		const modalBody = document.createElement('div');
-		modalBody.className = 'modal-body';
-		modalBody.innerHTML = `
-    <div class="product">
-        <img class="product-img" src="${product.img}" />
-        <div class="product-info">
-            <h4>${product.productName}</h4>
-        </div>
-      <div class="quantity">
-            <span class="quantity-btn-decrese">-</span>
-            <span class="quantity-input">${product.quanty}</span>"
-            <span class="quantity-btn-increse">+</span>
-      </div>
-            <div class="price">${product.price * product.quanty}$</div>
-            <div class="delete-product">❌</div>
-    </div>
-    `;
-		modalContainer.append(modalBody);
-
-		const decrese = modalBody.querySelector('.quantity-btn-decrese');
-		decrese.addEventListener('click', () => {
-			if (product.quanty !== 1) {
-				product.quanty--;
-				displayCart();
-			}
-		});
-
-		const increse = modalBody.querySelector('.quantity-btn-increse');
-		increse.addEventListener('click', () => {
-			product.quanty++;
-			displayCart();
-		});
-
-		// delete
-		const deleteProduct = modalBody.querySelector('.delete-product');
-
-		deleteProduct.addEventListener('click', () => {
-			deleteCartProduct(product.id);
-		});
-	});
-
-	// modal footer
-	const total = cart.reduce((acc, el) => acc + el.price * el.quanty, 0);
-
-	const modalFooter = document.createElement('div');
-	modalFooter.className = 'modal-footer';
-	modalFooter.innerHTML = `
-    <div class="total-price">Total: ${total}</div>
-    `;
-	modalContainer.append(modalFooter);
-=======
 	//modal Body
 	if (cart.length > 0) {
 		cart.forEach((product) => {
@@ -148,7 +87,6 @@ const displayCart = () => {
 		modalText.innerText = 'your cart is empty';
 		modalContainer.append(modalText);
 	}
->>>>>>> Stashed changes
 };
 
 cartBtn.addEventListener('click', displayCart);
