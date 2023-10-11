e9511d7a69f749dce78c8b8e86a56675ca821172
Eduardo Bru <113075379+EduardoBru@users.noreply.github.com>
2023-10-11 12:23:00 -0300

    6.2 Avanzamos desde una tupla

--- Python/Semana7/Clase01/dar_formato_str.py
+++ Python/Semana7/Clase01/dar_formato_str.py
@@ -3,4 +3,17 @@
 nombre = 'Ariel'
 edad = 28
 mensaje_con_formato = 'Mi nombre es %s y tengo %d años' % (nombre, edad)
-print(mensaje_con_formato)
+
+#Creamos una tupla
+persona = ('Noelia', 'Ruiz', 5000.00)
+mensaje_con_formato = 'Hola %s %s. Tu sueldo es %.2f' #% persona #le pasamos la tupla persona
+# print(mensaje_con_formato % persona) #podemos pasarlo en el print directamente
+
+nombre = 'Juan'
+edad = 19
+sueldo = 3000
+
+# se puede modificar el orden ya que manejamos índices.
+mensaje = 'Nombre {0} Edad {1} Sueldo {2:.2f}'.format(nombre, edad, sueldo)
+print(mensaje)
+
