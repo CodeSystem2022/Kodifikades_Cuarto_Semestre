6d715e701dcaab0ce2ed67a6a8d3d2acaa04251e
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-10-13 17:20:57 -0300

    8.4 Hacemos cambios hacía un algoritmo más complejo

--- Node/Semana8/02-Fundamentos_node/async/callback.js
+++ Node/Semana8/02-Fundamentos_node/async/callback.js
@@ -5,3 +5,28 @@ function soyAsincrona() {
 console.log('Iniciando el proceso...');
 soyAsincrona();
 console.log('Terminando el proceso...')
+
+
+function hola(nombre, miCallback){
+    setTimeout(() => {
+        console.log('Hola ' + nombre);
+        miCallback(nombre)
+    }, 1000);
+}
+
+function adios(nombre, otroCallback){
+    setTimeout(function() {
+        console.log("Adios", nombre);
+    }, 1000);
+}
+
+
+console.log('Iniciando el proceso...');
+hola('Carlos', function(nombre){
+    adios(nombre, function(){
+        console.log('Terminando el proceso...');
+    })
+})
+
+// hola('Carlos', function(){});
+// adios('Carlos', function(){});
