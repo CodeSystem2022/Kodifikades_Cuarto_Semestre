9d045636142a09fabbce4bf5fa7c3ac9146a3e0b
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-10-13 17:38:25 -0300

    8.3 Avanzamos con swing y la tabla -> JTable

--- Java/Semana8/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
+++ Java/Semana8/Leccion03/tienda_libros/src/main/java/utn/tienda_libros/vista/LibroFrom.java
@@ -5,12 +5,16 @@ import org.springframework.stereotype.Component;
 import utn.tienda_libros.servicio.LibroServicio;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 @Component
 public class LibroFrom extends JFrame {
     LibroServicio LibroServicio;
     private JPanel panel;
+    private JTable table1;
+
+    private DefaultTableModel tablaModeloLibros;
 
 
     @Autowired
@@ -31,6 +35,38 @@ public class LibroFrom extends JFrame {
         int y = (tamanioPantalla.height - getHeight()/2);
         setLocation(x, y);
     }
+
+    private void createUIComponents() {
+
+        this.tablaModeloLibros = new DefaultTableModel(0, 5);
+        String[] cabecera = {"Id", "Libro", "Autor", "Precio", "Existencias"};
+        this.tablaModeloLibros.setColumnIdentifiers(cabecera);
+
+        //Instanciar el objeto de JTable
+        this.table1 = new JTable(tablaModeloLibros);
+        listarLibros();
+
+    }
+
+    private void listarLibros(){
+        //Limpiar la tabla
+        tablaModeloLibros.setRowCount(0);
+
+        // Obtener los libros de la BD
+        var libros = LibroServicio.listarLibros();
+
+        // Iteramos cada libro
+        libros.forEach((libro) -> {
+            // Creamos cada registro para agregarlos a la tabla
+            Object[] renglonLibro = {
+                    libro.getIdLibro(),
+                    libro.getNombreLibro(),
+                    libro.getExistencias()
+            };
+            this.tablaModeloLibros.addRow(renglonLibro);
+        });
+    }
+
 }
 
 
